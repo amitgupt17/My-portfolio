4627b674789757063b3fb360de907e7140b908aa
Amit Gupta <amitgupt1719@gmail.com>
2025-12-14 21:52:09 +0530

    first commit

--- src/PersonalPortfolio.jsx
+++ src/PersonalPortfolio.jsx
@@ -1,7 +1,7 @@
 import About from "./component/About";
 import Contact from "./component/Contact";
 import Footer from "./component/Footer";
-import Hero from "./component/hero";
+import Hero from "./component/Hero";
 import Navbar from "./component/Navbar";
 import Project from "./component/Project";
 
